efb95505ab9f2f1811710468852adcc9c99852a2
Pavel Krush <pavel.krush@gmail.com>
2016-08-27 17:04:57 +0200

    initial commit

---
+++ control.lua
@@ -0,0 +1,89 @@
+if not pc then
+	pc = {
+		config = {
+			debug = false,
+			data_update_interval = 60,
+			pollution_values_count = 60 -- keep that count of pollution values
+		},
+		button = nil,
+		up = "↑",
+		down = "↓",
+		eq = "=",
+		current_pollution_index = 0,
+		pollution_values = {},
+		pollution_average = {},
+		pollution_deltas = {}
+	}
+end
+
+script.on_event(defines.events.on_tick, function()
+	if game.tick % pc.config.data_update_interval ~= 0 then
+		return
+	end
+
+	-- calculate pollution for all surfaces
+	for i, surface in pairs(game.surfaces) do
+		if not pc.pollution_values[surface.name] then
+			pc.pollution_values[surface.name] = {}
+			pc.pollution_average[surface.name] = 0.0
+			pc.pollution_deltas[surface.name] = 0.0
+			if pc.config.debug then
+				game.players[1].print(string.format("PC > Surface %s created, current index: %d", surface.name, pc.current_pollution_index))
+			end
+		end
+		local current_pollution = calc_pollution(surface)
+		pc.pollution_values[surface.name][pc.current_pollution_index] = current_pollution
+
+		-- recalculate average pollution value for surface
+		local sum = 0.0
+		for k, v in pairs(pc.pollution_values[surface.name]) do
+			sum = sum + v
+		end
+
+		if #pc.pollution_values[surface.name] > 0 then
+			pc.pollution_average[surface.name] = sum / #pc.pollution_values[surface.name]
+		end
+
+		if pc.pollution_average[surface.name] ~= 0.0 then
+			pc.pollution_deltas[surface.name] = (current_pollution - pc.pollution_average[surface.name]) / pc.pollution_average[surface.name]
+		end
+	end
+
+	-- Show pollution values
+	for i, player in pairs(game.players) do
+		if player.gui.top.pc_button == nil then
+			player.gui.top.add{type="button", name="pc_button"}
+		end
+
+		local is_changed = false
+		if math.abs(pc.pollution_deltas[player.surface.name]) > 0.01 then
+			is_changed = true
+		end
+
+		local char = pc.eq
+		if is_changed then
+			-- if current pollution is greater than average
+			if pc.pollution_values[player.surface.name][pc.current_pollution_index] > pc.pollution_average[player.surface.name] then
+				char = pc.up
+			else
+				char = pc.down
+			end
+		end
+		player.gui.top.pc_button.caption = string.format("Pollution: %.2f %s %.2f", pc.pollution_average[player.surface.name], char, pc.pollution_deltas[player.surface.name])
+	end
+
+	pc.current_pollution_index = pc.current_pollution_index + 1
+	if pc.current_pollution_index >= pc.config.pollution_values_count then pc.current_pollution_index = 0 end
+end)
+
+function calc_pollution(surface)
+	local pollution = 0.0
+	for coord in surface.get_chunks() do
+		pollution = pollution + surface.get_pollution({coord.x * 32, coord.y * 32})
+	end
+	return pollution
+end
+
+function isNAN(value)
+	return value ~= value
+end
